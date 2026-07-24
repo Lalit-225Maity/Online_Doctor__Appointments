a3efced9eb461003e2b7212cf44565ebf256c597
LALIT MAITY <maitylalit89@gmail.com>
2026-07-24 14:18:27 +0530

    Frontend Update

--- Frontend/doctor/vite.config.js
+++ Frontend/doctor/vite.config.js
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-     proxy:{
-      '/api':'https://online-doctor-appointments-3.onrender.com'
-     },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://online-doctor-appointments-3.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
   },
   plugins: [react()],
+
 })
