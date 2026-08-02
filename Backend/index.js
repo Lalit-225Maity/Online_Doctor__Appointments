b273bfaaf02f434e3a3a1c3d2e15974f7b5fa8f5
LALIT MAITY <maitylalit89@gmail.com>
2026-08-02 15:50:19 +0530

    Cors added

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,16 @@ const express=require('express');
 const dotenv=require('dotenv');
 const cookie = require('cookie-parser');
 const app=express();
+const cors = require("cors"); 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://online-doctor-appointments-8.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookie())
 dotenv.config();
