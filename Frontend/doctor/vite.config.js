204a86e35f0ac74cc4178eb1b4fd456a732895f8
LALIT MAITY <maitylalit89@gmail.com>
2026-08-02 15:11:41 +0530

    Frontend added

--- Frontend/doctor/vite.config.js
+++ Frontend/doctor/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
      proxy:{
-      '/api':'online-doctor-appointments-indol.vercel.app'
+      '/api':'https://doctor-online-appointment.onrender.com'
      },
   },
   plugins: [react()],
