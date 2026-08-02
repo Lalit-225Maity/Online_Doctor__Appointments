a99911c7913ec02d323b09a512271938b30153b8
LALIT MAITY <maitylalit89@gmail.com>
2026-08-02 14:54:55 +0530

    Hi Lalit

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,20 @@
-const app = require("./server");
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-module.exports = app; 
+const express=require('express');
+const dotenv=require('dotenv');
+const cookie = require('cookie-parser');
+const app=express();
+app.use(express.json());
+app.use(cookie())
+dotenv.config();
+const Database=require('./Database/db');
+Database();
+const DoctorDetails=require('./Routes/Doctors');
+app.use('/api',DoctorDetails);
+const User=require('./Routes/Customer');
+app.use('/api',User);
+const Appointment=require('./Routes/Appointment');
+app.use('/api',Appointment);
+const port=process.env.PORT;
+app.listen(port,()=>{
+    console.log(`server running at ${port}`);
+    
+})
